d9b26ebb5b4d3a5a994bec864bd8ad025edc2c48
noah <crashlive112@gmail.com>
2021-03-07 05:38:57 -0600

    fill out get and post login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,30 @@ class UsersController < ApplicationController
     erb :login
   end
 
+  # receive the login form,
+  # find the user, and log the user in (create session)
+  post '/login' do
+     # Find the user
+     @user = User.find_by(email: params[:email])
+     # Authenticate the user
+     if @user.authenticate(params[:password])
+     # log the user in - create user session
+     session[:user_id] = @user.id #actually logging the user in
+     # redirect the user's landing page
+     puts session
+     redirect "users/#{@user.id}"
+     else
+      # tell the user they entered invalid credentials
+      # redirect them to the login page
+     end
+  end
+
   get '/signup' do
-    
+
+  end
+
+  # user SHOW route
+  get '/users/:id' do
+    "this will be the user show route"
   end
 end
