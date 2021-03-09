f5a7877b89c9f682f93eda7aecc27e8c3d9254bc
noah <crashlive112@gmail.com>
2021-03-08 20:46:50 -0600

    add edit and update actions, with their routes

--- app/controllers/ecommerce_entries_controller.rb
+++ app/controllers/ecommerce_entries_controller.rb
@@ -23,14 +23,52 @@ class EcommerceEntriesController < ApplicationController
 
   # show page for ecommerce entry
   get '/ecommerce_entries/:id' do
-    @ecommerce_entry = EcommerceEntry.find(params[:id])
+    set_ecommerce_entry
     erb :"ecommerce_entries/show"
   end
 
   # This route should send us to eccomerce_entries/edit.erb
   # render an edit form
   get '/ecommerce_entries/:id/edit' do
-    erb :'/ecommerce_entries/edit'
+    # find entry to edit
+    set_ecommerce_entry
+    # only logged in users can edit posts
+    if logged_in?
+    # verify current user is the same as original entry user
+      if @ecommerce_entry.user == current_user
+        erb :'/ecommerce_entries/edit'
+      else
+        redirect "users/#{current_user.id}"
+      end
+      else
+        # send them to homepage if not logged in
+        redirect "/"
+    end
+  end
+ 
+  patch '/ecommerce_entries/:id' do
+    # 1. find ecommerce entry
+    set_ecommerce_entry
+    if logged_in?
+      # verify current user is the same as original entry user
+      if @ecommerce_entry.user == current_user
+        # 2. update entry
+        @ecommerce_entry.update(content: params[:content])
+        # 3. redirect to show page
+        redirect "ecommerce_entries/#{@ecommerce_entry.id}"
+      else
+        redirect "users/#{current_user.id}"
+      end
+      else
+        # send them to homepage if not logged in
+        redirect "/"
+    end
   end
   # index route for all ecommerce entries
+
+  private
+
+  def set_ecommerce_entry
+    @ecommerce_entry = EcommerceEntry.find(params[:id])
+  end
 end
