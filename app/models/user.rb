3edbd8c746eecf3626f0b7a7af77f6e79b685eea
noah <crashlive112@gmail.com>
2021-03-11 02:02:43 -0600

    add AR validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ActiveRecord::Base
   has_secure_password
 
+  # Prevent creating, saving, or updating if these req. are not met
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  
   has_many :ecommerce_entries
   
 end
