397cccfb85cad1cf239c51a644418901364f9e1e
noah <crashlive112@gmail.com>
2021-03-13 06:10:23 -0600

    add validations to ecommerce entries

--- app/models/ecommerce_entry.rb
+++ app/models/ecommerce_entry.rb
@@ -2,4 +2,9 @@ class EcommerceEntry < ActiveRecord::Base
 
   belongs_to :user
   
+   # Prevent creating, saving, or updating if these req. are not met
+   validates :title, presence: true
+   validates :price, presence: true
+   validates :content, presence: true
+
 end
