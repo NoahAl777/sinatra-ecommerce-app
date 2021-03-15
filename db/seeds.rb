54512a04285fdaebf47ae9c1a237713adc892c47
noah <crashlive112@gmail.com>
2021-03-15 11:54:08 -0500

    db changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,16 +2,16 @@
 
 # Create 2 users
 
-noah = User.create(name: "Noah", email: "noah@noah.com", password: "password")
-lex = User.create(name: "Lex", email: "lex@lex.com", password: "password")
+mark = User.create(name: "Mark", email: "mark@mark.com", password: "password")
+jess = User.create(name: "Jess", email: "jess@jess.com", password: "password")
 
 # Create some ecommerce entries
 
-EcommerceEntry.create(title: "Make your car faster today!", price: 5000, content: "Turbo charger", user_id: noah.id)
+EcommerceEntry.create(title: "Make your car faster today!", price: 5000, content: "Turbo charger", user_id: mark.id)
 
 # Use AR to pre-associate data:
 
-noah.ecommerce_entries.create(title: "Get a new air filter now!", price: 75, content: "Best air filter on the market!")
+mark.ecommerce_entries.create(title: "Get a new air filter now!", price: 75, content: "Best air filter on the market!")
 
-lexs_entry = lex.ecommerce_entries.build(title: "Is your car having problems?", price: 0, content: "Free diagnostics at all of our locations!")
-lexs_entry.save
+jess_entry = jess.ecommerce_entries.build(title: "Is your car having problems?", price: 0, content: "Free diagnostics at all of our locations!")
+jess_entry.save
